4397ccd8557b19167cf5dcb5488bf40c58d92f2a
Vishal Gondke <vishalgondke@gmail.com>
2022-11-01 15:36:32 +0530

    truffle installation

---
+++ truffleTutorial/New/contracts/credit.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: GPL-3.0
+//Vishal Gondke 2019140025 BE IT
+pragma solidity ^0.8.17;
+
+contract LotteryVishal {
+    uint creditLimit = 10000;
+    constructor(){
+        creditLimit=10000;
+    }
+    function getCreditLimit()public view returns (uint){
+        return creditLimit;
+    }
+
+    function expenditure(uint travelCost,uint stayCost, uint foodCost)public{
+        uint totalExpenditure= travelCost+foodCost+stayCost;
+        require(creditLimit-totalExpenditure>=0,"Total cost exceeded the credit limit");
+        creditLimit=creditLimit-totalExpenditure;
+    }
+    
+    function resetCreditLimit() public{
+        creditLimit=10000;
+    }
+}
